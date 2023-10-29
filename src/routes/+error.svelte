e30347240ac076612f6fde8ea9ba6a6c84264cea
elvinos <elvinos33@duck.com>
2023-10-29 13:56:34 +0100

    custom 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,5 @@
+<div class="absolute inset-0 flex flex-col items-center justify-center">
+  <h1 class="h1">404</h1>
+  <h2 class="h2">Page not found.</h2>
+  <h3 class="h3">Please try another path</h3>
+</div>
