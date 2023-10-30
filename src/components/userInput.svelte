69c4fc537831e9fa395e94e760a18a4b517bc77a
elvinos <elvinos33@duck.com>
2023-10-30 18:45:30 +0100

    argument for div and no select on empty array

--- src/components/userInput.svelte
+++ src/components/userInput.svelte
@@ -6,6 +6,7 @@
     icon: string,
     selection: Array<string>,
     selectDefault: string,
+    divArg: string,
   }
 </script>
 
@@ -22,13 +23,14 @@
 <div class="flex w-full flex-col items-center gap-8">
   <h4 class="h4">{inputProps.title}</h4>
   <form class="flex w-1/3 min-w-[300px]" on:submit={e => {e.preventDefault(); onSubmit(input, selected); input = '';} }>
-    {#if inputProps.selection}
+    {#if inputProps.selection.length > 0}
       <select bind:value={selected} class="w-fit select variant-ghost" name="" id="">
         {#each inputProps.selection as select}
           <option value="{select}">{select}</option>
         {/each}
       </select>
     {/if}
+    <div class="w-fit input variant-ghost p-5 pr-0">{inputProps.divArg}</div>
     <input disabled={isDisabled} bind:value={input} autofocus required type="text" class="peer input variant-ghost w-full p-5" placeholder="{inputProps.placeholder}">
     <button disabled={input === '' || isDisabled} class="btn variant-filled-secondary">
       <Icon icon="{inputProps.icon}" />
