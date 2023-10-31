0fcb069771c7cfffdc8212b221fc8d3a3400d869
Elvin Baloku <elvinos33@duck.com>
2023-10-31 11:38:19 +0100

    Support for sharepoint

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,12 +1,18 @@
- export function isValidURL(url: string) {
-    var urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
-	    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
-	    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
-	    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
-	    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
-	    '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
-	  return !!urlPattern.test(url);
-  }
+export function isValidURL(url: string) {
+  // Regular expression to check for SharePoint URLs
+  const sharePointPattern = new RegExp('^(https?://)?([a-zA-Z0-9-]+\\.sharepoint\\.com).*','i');
+
+  // Regular expression to check for a general URL
+  const urlPattern = new RegExp('^(https?://)?'+ // validate protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+    '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+
+  return !!urlPattern.test(url) || !!sharePointPattern.test(url);
+}
+
 
   export async function doesPathExist(path: string) {
     const response = await fetch(`/?path=${path}`)
