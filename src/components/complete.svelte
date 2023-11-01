fb905b8d572ace2d0e17770931aa4968cfcf93d2
Elvin Baloku <elvinos33@duck.com>
2023-11-01 16:21:07 +0100

    ghost for bigger screens on finished lin

--- src/components/complete.svelte
+++ src/components/complete.svelte
@@ -9,7 +9,7 @@
 <div class="w-full flex flex-col items-center gap-8">
   <h1 class="h1">Hooray!</h1> 
   <h4 class="h4">Your new shortened URL is</h4> 
-  <div class="card input md:variant-ghost !bg-transparent p-5 flex md:flex-row flex-col w-1/3 justify-between items-center gap-4">
+  <div class="card input md:bg-opacity-100 variant-ghost bg-opacity-0 p-5 flex md:flex-row flex-col w-1/3 justify-between items-center gap-4">
     <a href={`https://${finalURL}`} class="h5">{finalURL}</a>
     <button class="btn h-full variant-filled-secondary" use:clipboard={finalURL}>
       Copy
